b4d0e664ff1b4fadfa6a1383944f5d3bbebd62e9
Nirocon <166694799+Nirocon@users.noreply.github.com>
2025-08-07 15:37:04 +0900

    update: プログラムの編集を容易にするため、アカウント名やサーバーディレクトリーを環境変数として保持するようにした

--- src/bot.py
+++ src/bot.py
@@ -4,39 +4,38 @@ from dotenv import load_dotenv
 import subprocess
 import os
 
-load_dotenv(dotenv_path=os.path.expanduser('../.env')) # .envファイルから環境変数を読み込む
-TOKEN = os.getenv('DISCORD_TOKEN') # トークン取得
+load_dotenv(dotenv_path=os.path.expanduser('.env')) # .envファイルから環境変数を読み込む
+TOKEN                       = os.getenv('DISCORD_TOKEN') # トークン取得
+MINECRAFT_CONTROLL_ACCOUNT  = os.getenv('MINECRAFT_CONTROLL_ACCOUNT')  # マイクラサーバーを実行しているユーザー名
+MINECRAFT_SERVER_DER_PATH   = os.getenv('SERVER_DIR_PATH')  # サーバーのパス
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-minecraft_user = "minecraft_controll_account"  # マイクラサーバーを実行しているユーザー名
-minecraft_server_path = "~/bedrock-server"  # サーバーのパス
-
 # マイクラサーバーを起動する関数
 def start_minecraft_server():
-    command = f"sudo -u {minecraft_user} bash -c 'whoami && cd {minecraft_server_path} && LD_LIBRARY_PATH=. screen -dmS minecraft_server ./bedrock_server'"
+    command = f"sudo -u {MINECRAFT_CONTROLL_ACCOUNT} bash -c 'whoami && cd {MINECRAFT_SERVER_DER_PATH} && LD_LIBRARY_PATH=. screen -dmS minecraft_server ./bedrock_server'"
     subprocess.run(command, shell=True)
 
 # マイクラサーバーを停止する関数
 def stop_minecraft_server():
     try:
         # サーバーを停止するために`stop`コマンドを送る
-        subprocess.run(f"sudo -u {minecraft_user} screen -S minecraft_server -p 0 -X stuff 'stop\n'", shell=True)
+        subprocess.run(f"sudo -u {MINECRAFT_CONTROLL_ACCOUNT} screen -S minecraft_server -p 0 -X stuff 'stop\n'", shell=True)
     except Exception as e:
         print(f"Error stopping Minecraft server: {e}")
 
 # スラッシュコマンド登録
-@bot.tree.command(name="start", description="マイクラサーバーを起動します")
+@bot.tree.command(name="start-test", description="マイクラサーバーを起動します")
 async def start(interaction: discord.Interaction):
     """Minecraftサーバーを起動するコマンド"""
     await interaction.response.send_message("Minecraftサーバーを起動します...")
     start_minecraft_server()
     await interaction.followup.send("Minecraftサーバーが起動しました！")
 
-@bot.tree.command(name="stop", description="マイクラサーバーを停止します")
+@bot.tree.command(name="stop-test", description="マイクラサーバーを停止します")
 async def stop(interaction: discord.Interaction):
     """Minecraftサーバーを停止するコマンド"""
     await interaction.response.send_message("Minecraftサーバーを停止します...")
